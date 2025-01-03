a45b7f9ba267c598d2c2283a28cdeba4a3a10682
garvit2004 <garvit.soni.1404@gmail.com>
2025-01-03 21:27:27 +0530

    first commit

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Pattern10
+{
+    public static void main(String[] args)
+    {
+       Pattern2.main(args);
+       Pattern5.main(args);
+    }
+}
+
+/*
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ */
---
+++ Patterns/Pattern9.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Pattern9
+{
+    public static void main(String[] args)
+    {
+        Pattern7.main(args);
+        Pattern8.main(args);
+    }
+}
+
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *
+ */
